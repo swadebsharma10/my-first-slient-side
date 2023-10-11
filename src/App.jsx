7964dedb92d686bdf3da44d0dcd58a345a431ad7
swadebsharma6 <swadebriva2012@gmail.com>
2023-10-11 16:18:46 +0600

    adeed handleSubmit to send data on the backend

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,65 @@ function App() {
     .then(res => res.json())
     .then(data => setUsers(data))
 
- }, [])
+ }, []);
+
+
+ const handleSubmit =(event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        
+        const user ={name, email}
+
+      //  post data on the server side
+      fetch('http://localhost:5000/users',{
+        method: 'POST',
+        headers:{
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+
+       })
+       
+        
+ }
 
   return (
-    <div>
-      <h1>Users management system: {users.length}</h1>
-      <div>
-        {users.map(user => <p key={user.id}>{user.id} {user.name} {user.email}</p> )}
-      </div>
-    </div>
+   <section>
+   <div>
+   <h1 className='text-center font-bold text-4xl text-primary my-10'>Users management system: {users.length}</h1>
+   <div>
+     {users.map(user => <p className='text-center text-xl font-bold' key={user.id}>{user.id} {user.name} {user.email}</p> )}
+   </div>
+ </div>
+
+ <div className='mt-10'>
+ <div className="hero">
+ <div className="hero-content flex-col">
+   <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+     <form  onSubmit={handleSubmit} className="card-body">
+       <div className="form-control">
+         <label className="label">
+           <span className="label-text">Name</span>
+         </label>
+         <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+       </div>
+       <div className="form-control">
+         <label className="label">
+           <span className="label-text">Email</span>
+         </label>
+         <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+       </div>
+       <div className="form-control mt-6">
+         <button type='submit' className="btn btn-primary">Submit</button>
+       </div>
+     </form>
+   </div>
+ </div>
+</div>
+ </div>
+   </section>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <div className='max-w-6xl mx-auto'>
+   <App />
+   </div>
   </React.StrictMode>,
 )
